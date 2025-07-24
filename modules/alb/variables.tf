1a5c6442448533c383a424a7b39e0b63d803f1ff
Jegan <118361805+Jegan-1995@users.noreply.github.com>
2025-07-24 14:50:31 +0530

    Create variables.tf

---
+++ modules/alb/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "Public subnet IDs"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+}
