97c0b5416ef8c1dd88ec684bd699bd0aaa50dc55
Jegan <118361805+Jegan-1995@users.noreply.github.com>
2025-07-24 15:28:25 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,25 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "alb_dns_name" {
+  description = "ALB DNS name"
+  value       = module.alb.dns_name
+}
+
+output "alb_url" {
+  description = "Application URL"
+  value       = "http://${module.alb.dns_name}"
+}
+
+output "ecs_cluster_name" {
+  description = "ECS Cluster name"
+  value       = module.ecs.cluster_name
+}
+
+output "rds_endpoint" {
+  description = "RDS endpoint"
+  value       = module.rds.endpoint
+  sensitive   = true
+}
